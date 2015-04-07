5faae08e6734da26366e8d7d0ec789307a8452b7
Amanda Kom <mandareneekom@gmail.com>
2015-04-07 16:47:15 -0700

    Adding Queue test and code

---
+++ lib/queue.rb
@@ -0,0 +1,21 @@
+Node = Struct.new(:val, :nxt)
+
+class Queue
+  attr_accessor :head, :tail, :size
+
+  def enqueue(val)
+    node = Node.new(val, nil)
+    @head, @tail, @size = node, node, 0 if @head.nil?
+    @size += 1
+    @tail.nxt = node
+    @tail = node
+  end
+
+  def dequeue
+    raise "No queue object" if @head.nil?
+    @size -= 1
+    temp = @head.val
+    @head = @head.nxt
+    temp
+  end
+end
